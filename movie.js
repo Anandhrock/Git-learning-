73937e2770a3fb2356d35180f172c1327a03de71
Anandhrock <anandhrock91@gmail.com>
2024-06-04 13:21:28 +0530

    this movie

---
+++ movie.js
@@ -0,0 +1,27 @@
+class movie {
+
+    constructor(title = " ", studion = " ", rating = "PG") {
+
+        this.title = title;
+        this.studio = studio;
+        this.rating = rating;
+    }
+    
+}
+getPG(movies = [movie]){
+    var filterarray [ ]
+    movie.forEach(movie)=>{
+        if (movie.rating = "pg") {
+
+            filterarray.push(movie)
+
+            
+        }
+    }
+    return [];
+}
+console.log(movie)
+const spd1 = new movie("spd", "eros");
+const avenger = new movie("avenger", "eros");
+const spd2 =new movie("spd","eros","pg1")
+    
